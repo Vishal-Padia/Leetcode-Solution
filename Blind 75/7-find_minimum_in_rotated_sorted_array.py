7c049d633f5a85f65a1680e302db441cead80a4f
Vishal-Padia <vishalpadia9@gmail.com>
2024-02-29 11:45:42 +0530

    formatting

--- Blind 75/7-find_minimum_in_rotated_sorted_array.py
+++ Blind 75/7-find_minimum_in_rotated_sorted_array.py
@@ -6,7 +6,7 @@ class Solution:
 
         # Binary Search
         l, r = 0, len(nums) - 1  # initialize l,r to 0 and len(nums)-1
-        while nums[r] < nums[l]:  # whilet the nums[r] element is less than nums[l]
+        while nums[r] < nums[l]:  # while the nums[r] element is less than nums[l]
             mid = (l + r) // 2  # get the middle element using  (l+r)//2
             if nums[mid] < nums[r]:  # check if the nums[mid] is less than nums[r]
                 r = mid  # if it is then update the r to mid
